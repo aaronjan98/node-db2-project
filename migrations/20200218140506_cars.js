7edb0d1bf49f7f10203c6cdb3f28016919946d09
aaronjan <aaronjan98@gmail.com>
2020-02-18 14:48:28 -0800

    made the table
    first run: knex migrate:latest

--- migrations/20200218140506_cars.js
+++ migrations/20200218140506_cars.js
@@ -1,8 +1,17 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('cars', tbl => {
+
+      tbl.string('VIN', 17).primary().notNullable();
+      tbl.string('make', 255).notNullable().index();
+      tbl.string('model', 255).notNullable().index();
+      tbl.string('mileage').notNullable();
+
+      tbl.string('transmission', 255);
+      tbl.string('status', 255);
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('cars');
 };
